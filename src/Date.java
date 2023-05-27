f4c419d96e5a624baef0d205907350d7c6a7a174
rai-stein <raissacs2003@gmail.com>
2023-05-27 17:44:42 +0300

    Part A - with comments

--- src/Date.java
+++ src/Date.java
@@ -2,14 +2,41 @@ public class Date {
     int day = 1; // between 1 and 31
     int month = 1; // between 1 and 12
     int year = 0; // between -3999 to 3999
+    /* Constructor for the Class Date
+     */
     public Date(int y, int m, int d) {
         setDay(d);
         setMonth(m);
         setYear(y);
 
     }
+    /* Checks that the month is legal, and if so sets the object's month
+     */
+    public void setMonth(int i) {
+        if((1 <= i) && (i <= 12)){
+            this.month = i;
+        }
+    }
+    /* Checks that the day is legal, and if so sets the object's day
+     */
+    public void setDay(int i){
+        if((1 <= i) && (i <= 31)){
+            this.day = i;
+        }
+    }
+    /* Checks that the year is legal, and if so sets the object's year
+     */
+    public void setYear(int i){
+        if((-3999 <= i) && (i <= 3999)){
+            this.year = i;
+        }
+    }
+    /* overriding the Object's toString function, returns our DateTime in the desired format
+     * DD/MM/YYYY
+     */
+    @Override
     public String toString(){
-        String toReturn = " ";
+        String toReturn;
         String day = Integer.toString(this.day);
         String month = Integer.toString(this.month);
         String year = Integer.toString(this.year);
@@ -22,21 +49,8 @@ public class Date {
         toReturn = day + "/" + month + "/" + year;
         return toReturn;
     }
-    public void setMonth(int i) {
-        if((1 <= i) && (i <= 12)){
-            this.month = i;
-        }
-    }
-    public void setDay(int i){
-        if((1 <= i) && (i <= 31)){
-            this.day = i;
-        }
-    }
-    public void setYear(int i){
-        if((-3999 <= i) && (i <= 3999)){
-            this.year = i;
-        }
-    }
+    /* overriding the Object's equals function, returns whether two objects are the same, or have the same values!
+     */
     @Override
     public boolean equals(Object o) {
         if(!(o instanceof Date)){
@@ -48,7 +62,9 @@ public class Date {
         }
         return (this.hashCode() == otherDate.hashCode());
     }
-
+    /* overriding the Object's hashCode function, returns a unique number for each of the objects
+     * depending solely on its characteristics values
+     */
     @Override
     public int hashCode(){
         String day = Integer.toString(this.day);
--- src/DateTime.java
+++ src/DateTime.java
@@ -1,26 +1,35 @@
 public class DateTime extends Date {
     int hour = 0; // between 00 and 23
     int min = 0; // between 00 and 59
+    /* Constructor for the subClass DateTime
+     * utilizes the constructor in the superClass Date
+     */
     public DateTime(int y, int m, int d, int h, int min) {
         super(y, m, d);
         setHour(h);
         setMinute(min);
     }
+    /* Checks that the hour is legal, and if so sets the object's hour
+     */
     public void setHour(int i) {
         if((0 <= i) && (i <= 23)){
             this.hour = i;
         }
     }
+    /* Checks that the minute is legal, and if so sets the object's minute
+     */
     public void setMinute(int i) {
         if((0 <= i) && (i <= 59)){
             this.min = i;
         }
     }
+    /* overriding the Object's toString function, returns our DateTime in the desired format
+     * DD/MM/YYYY hh:mm
+     */
     @Override
     public String toString(){
-        String toReturn = " ";
+        String toReturn = super.toString() + " ";
         // DD/MM/YYYY hh:mm
-        toReturn = super.toString() + " ";
         // here we have to use the toString in the Super Class to get the data format
         // and then add to it the part of "hh:mm"
         String hour = Integer.toString(this.hour);
@@ -30,6 +39,8 @@ public class DateTime extends Date {
         toReturn = toReturn + hour + ":" + minute;
         return toReturn;
     }
+    /* overriding the Object's equals function, returns whether two objects are the same, or have the same values!
+     */
     @Override
     public boolean equals(Object o){
         if(!(o instanceof DateTime)){
@@ -42,7 +53,10 @@ public class DateTime extends Date {
 
         return (this.hashCode() == otherDate.hashCode());
     }
-
+    /* overriding the Object's hashCode function, returns a unique number for each of the objects
+     * depending solely on its characteristics values
+     * also overrides the superClass's function hashCode / utilizes it
+     */
     @Override
     public int hashCode(){
         int hashNumDate = super.hashCode();
