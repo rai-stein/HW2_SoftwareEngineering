be54d9cb87adcc2cd351473d5dc0bfd645154888
rai-stein <raissacs2003@gmail.com>
2023-05-27 16:14:52 +0300

    toString function in Date and DateTime

--- src/Date.java
+++ src/Date.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Date {
+public class Date {
+    int day; // between 1 and 31
+    int month; // between 1 and 12
+    int year; // between -3999 to 3999
+    public Date(int y, int m, int d) {
+
+    }
+    public String toString(){
+        String toReturn = " ";
+        String day = Integer.toString(this.day);
+        String month = Integer.toString(this.month);
+        String year = Integer.toString(this.year);
+        if(day.length() == 1) day = "0" + day;
+        if(month.length() == 1) month = "0" + month;
+        if(year.length() == 1) year = "000" + year;
+        else if(year.length() == 2) year = "00" + year;
+        else if(year.length() == 3) year = "0" + year;
+        // DD/MM/YYYY
+        toReturn = day + "/" + month + "/" + year;
+        return toReturn;
+    }
+
+    @Override
+    public boolean equals(Date o) {
+        if(!(o instanceof this)){
+            return false;
+        }
+
+    }
+
+
 }
--- src/DateTime.java
+++ src/DateTime.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class DateTime {
+public class DateTime extends Date {
+    int hour; // between 00 and 23
+    int min; // between 00 and 59
+    public DateTime(int y, int m, int d, int h, int min) {
+        super(y, m, d);
+        this.hour = h;
+        this.min = min;
+    }
+
+    public void setHour(int i) {
+    }
+
+    public void setMonth(int i) {
+    }
+
+    public void setMinute(int i) {
+    }
+
+    @Override
+    public String toString(){
+        String toReturn = " ";
+        // DD/MM/YYYY hh:mm
+        toReturn = super.toString() + " ";
+        // here we have to use the toString in the Super Class to get the data format
+        // and then add to it the part of "hh:mm"
+        String hour = Integer.toString(this.hour);
+        String minute = Integer.toString(this.min);
+        if(hour.length() == 1) hour = "0" + hour;
+        if(minute.length() ==1) minute = "0" + minute;
+        toReturn = toReturn + hour + ":" + minute;
+        return toReturn;
+    }
+
+//    @Override
+//    public boolean equals(DateTime otherDate){
+//        boolean equals = true;
+//
+//        return equals;
+//    }
+
 }
